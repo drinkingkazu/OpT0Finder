446b53244140a9a3bcda181da7328ffba29e049d
drinkingkazu <kazuhiro@nevis.columbia.edu>
2019-10-22 16:23:57 +0000

    minor: moving around things for python imports, also take out duplicate class loading in pyutil that could conflict w/ larcv

--- flashmatch/PyUtil/LinkDef.h
+++ flashmatch/PyUtil/LinkDef.h
@@ -13,8 +13,6 @@
 // Functions
 //
 #ifndef __CINT__
-#pragma link C++ function larcv::as_ndarray(const larcv::Image2D&)+;
-#pragma link C++ function larcv::as_image2d(PyObject*,size_t,size_t)+;
 #pragma link C++ function larcv::copy_array(PyObject*, const std::vector< short              > &)+;
 #pragma link C++ function larcv::copy_array(PyObject*, const std::vector< unsigned short     > &)+;
 #pragma link C++ function larcv::copy_array(PyObject*, const std::vector< int                > &)+;
@@ -23,8 +21,6 @@
 #pragma link C++ function larcv::copy_array(PyObject*, const std::vector< unsigned long long > &)+;
 #pragma link C++ function larcv::copy_array(PyObject*, const std::vector< float              > &)+;
 #pragma link C++ function larcv::copy_array(PyObject*, const std::vector< double             > &)+;
-#pragma link C++ function larcv::fill_img_col( Image2D&, std::vector<short>&, const int, const int, const float pedestal=0.0);
-#pragma link C++ function larcv::play(PyObject*)+;
 //#pragma link C++ function larcv::as_mat(const larcv::Image2D&)+;
 //#pragma link C++ function larcv::as_ndarray<short>(const std::vector<short>&)+;
 #pragma link C++ function larcv::as_ndarray (const std::vector< short              >& vec)+;
@@ -36,8 +32,6 @@
 #pragma link C++ function larcv::as_ndarray (const std::vector< float              >& vec)+;
 #pragma link C++ function larcv::as_ndarray (const std::vector< double             >& vec)+;
 #endif
-#pragma link C++ class larcv::load_pyutil+;
-#pragma link C++ class larcv::PyImageMaker+;
 //ADD_NEW_CLASS ... do not change this line
 
 #endif
